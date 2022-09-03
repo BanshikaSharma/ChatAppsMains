a5212b0e7a9c70387e408f5bbca82034d96a81c5
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-09-03 22:10:20 +0530

    styled components on Register

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -1,7 +1,56 @@
-import React from 'react'
+import React from "react";
+import styled from "styled-components";
+import {Link} from 'react-router-dom'
 
 export default function Register() {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
+
+  const handleChange = (event) => {
+    console.log(event)
+  };
+
   return (
-    <div>Register</div>
-  )
+    <>
+      <FormContainer>
+        <form onSubmit={(event) => handleSubmit(event)}>
+          <div className="brand">
+            {/* <img src="#" atl="This is image" /> */}
+            <h1>Snappy</h1>
+          </div>
+          <input
+            type="text"
+            placeholder="Username"
+            name="Username"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="email"
+            placeholder="Email"
+            name="email"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            onChange={(e) => handleChange(e)}
+          />
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="confirmPassword"
+            onChange={(e) => handleChange(e)}
+          />
+          <button type="submit">Create User</button>
+          <span>
+            Already have an account ? <Link to="/login">Login</Link>
+          </span>
+        </form>
+      </FormContainer>
+    </>
+  );
 }
+
+const FormContainer = styled.div``;
