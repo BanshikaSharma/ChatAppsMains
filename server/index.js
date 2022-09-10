6ad0cf2b69686e047f279a8d8b16ef0e96373d08
Mohammad Arsalan <arsalanmohd237@gmail.com>
2022-09-10 17:34:11 +0530

    socket server side

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const mongoose = require("mongoose");
 const userRoutes = require("./routes/userRoutes");
 const messageRoute = require("./routes/messagesRoute");
+const socket = require("socket.io");
 
 const app = express();
 require("dotenv").config();
@@ -27,4 +28,27 @@ mongoose
 
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server Started on ${process.env.PORT}`);
+});
+
+const io = socket(server, {
+  cors: {
+    origin: "https://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.msg);
+    }
+  });
 });
